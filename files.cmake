f8c5793fd7911dadc0e8ea2e7caade9adcc00029
ZoeChengYu <0931426632yang50@gmail.com>
2026-03-26 16:43:41 +0800

    新增: 更新 Gate 檔案至 SRC_FILES 和 INCLUDE_FILES

--- files.cmake
+++ files.cmake
@@ -2,12 +2,14 @@ set(SRC_FILES
     App.cpp
     Util/AtlasLoader.cpp
     Util/Enemy.cpp
+    Util/Gate.cpp
 )
 
 set(INCLUDE_FILES
     App.hpp
     Util/AtlasLoader.hpp
     Util/Enemy.hpp
+    Util/Gate.hpp
 )
 
 set(TEST_FILES
